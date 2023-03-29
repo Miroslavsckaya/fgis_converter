b60e816397f0c41030a3dfffee646f3b70071d04
mitsuha_s <stmiroslavsckaya@ya.ru>
2023-03-29 22:03:50 +0300

    gui.py removed, url parser added

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #! python
 import argparse
 from converter import convert_csv_to_xml_file
-import gui
+import logging
+import PySimpleGUIQt as sg
+from sys import exit
+from urllib.parse import urlparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('path_input', default=None, nargs='?')
@@ -9,7 +12,12 @@ parser.add_argument('path_output', default='./application', nargs='?')
 args = vars(parser.parse_args())
 
 if args['path_input'] is None:
-    window = gui.GuiFrontend()
-    window.Start()
+    file_path = sg.popup_get_file('Выберите файл для конвертации', title='Аршин', keep_on_top=True)
+    url = urlparse(file_path, allow_fragments=False)
+    if url.scheme in ('file', ''):
+        convert_csv_to_xml_file(file_path)
+    else:
+        logging.error('unsupported scheme')
+        exit(1)
 else:
     convert_csv_to_xml_file(args['path_input'], args['path_output'])
