485ff7ebcd108dec37cfea87ef29c614104f4750
mitsuha_s <stmiroslavsckaya@ya.ru>
2023-04-20 21:19:46 +0300

    reworked VerificationData.__init__

--- data_sources/interface.py
+++ data_sources/interface.py
@@ -1,18 +1,31 @@
 from abc import ABC, abstractmethod
-from exceptions import VerificationKeyError
 from typing import Generator
 
 
+class DataSourceInterface(ABC):
+    @abstractmethod
+    def get_name(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_verification_generator(self, file_path: str) -> Generator[VerificationData, None, None]:
+        pass
+
+
 class VerificationData:
-    def __init__(self, info: dict) -> None:
-        """Expected keys in the dict"""
-        keys = ['reg_num', 'factory_num', 'modification', 'ver_date', 'valid_date',
-                'metrologist', 'test_dev_num', 'pressure', 'temperature', 'humidity']
-        for key in keys:
-            try:
-                self.__dict__[f'_VerificationData__{key}'] = info[key]
-            except KeyError:
-                raise VerificationKeyError('Missing key:', key)
+    def __init__(self, reg_num: str, factory_num: str, modification: str, ver_date: str,
+                 valid_date: str, metrologist: str, test_dev_num: str, pressure: str,
+                 temperature: str, humidity: str) -> None:
+        self.__reg_num: str = reg_num
+        self.__factory_num: str = factory_num
+        self.__modification: str = modification
+        self.__ver_date: str = ver_date
+        self.__valid_date: str = valid_date
+        self.__metrologist: str = metrologist
+        self.__test_dev_num: str = test_dev_num
+        self.__pressure: str = pressure
+        self.__temperature: str = temperature
+        self.__humidity: str = humidity
 
     @property
     def reg_num(self):
@@ -53,14 +66,3 @@ class VerificationData:
     @property
     def humidity(self):
         return self.__humidity
-
-
-class DataSourceInterface(ABC):
-    @abstractmethod
-    def get_name(self) -> str:
-        pass
-
-    @abstractmethod
-    def get_verification_generator(self, file_path: str) -> Generator[VerificationData, None, None]:
-        pass
-
