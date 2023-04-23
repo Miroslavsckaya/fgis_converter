4ee2db342b0706dcff632f59246f6dc2b0a2e002
mitsuha_s <stmiroslavsckaya@ya.ru>
2023-04-23 13:47:26 +0300

    implemented convert_to_verification_data in CsvDataSource

--- data_sources/csv.py
+++ data_sources/csv.py
@@ -1,6 +1,7 @@
 import csv
+import logging
 from data_sources.base import BaseDataSource
-from interface import VerificationData
+from data_sources.interface import VerificationData
 from typing import Generator
 
 
@@ -13,8 +14,15 @@ class CsvDataSource(BaseDataSource):
         file = self.open_file(file_path, 'r')
         reader = csv.reader(file, delimiter=self.__delimiter)
         for row in reader:
-            yield self.convert_to_verification_data(row)
+            yield self.convert_to_verification_data(row, reader.line_num)
         file.close()
 
-    def convert_to_verification_data(self, row: list[str]) -> VerificationData:
-        pass
+    @staticmethod
+    def convert_to_verification_data(row: list[str], line_num: int) -> VerificationData | None:
+        """Expected order:
+            1) registration number, 2) factory number, 3) modification, 4) verification date, 5) valid date,
+            6) metrologist, 7) test device number, 8) temperature, 9) pressure, 10) humidity"""
+        if not all(row[:4] + row[5:]):
+            logging.warning(f'Строка №{line_num} не может быть cконвертирована. Все поля должны быть заполнены')
+            return None
+        return VerificationData(*row)
