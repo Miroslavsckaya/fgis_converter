b19d19ef4c603de7af190c9a9234861f34fe1fd0
mitsuha_s <stmiroslavsckaya@ya.ru>
2023-06-12 15:43:36 +0300

    extension parameter renamed to new in PathHelper.replace_extension method

--- path_helper.py
+++ path_helper.py
@@ -7,7 +7,7 @@ class PathHelper:
         return str(pathlib.Path(relative).resolve())
 
     @staticmethod
-    def replace_extension(input_path: str, extension: str) -> str:
+    def replace_extension(input_path: str, new: str) -> str:
         if not input_path:
             return ''
-        return str(pathlib.Path(input_path).with_suffix(extension))
+        return str(pathlib.Path(input_path).with_suffix(new))
