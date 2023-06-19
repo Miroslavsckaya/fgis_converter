68ea63584f21ca7824b552054df7d6706ba9429a
mitsuha_s <stmiroslavsckaya@ya.ru>
2023-06-19 16:06:51 +0300

    PurePath changed to Path in PathHelper

--- path_helper.py
+++ path_helper.py
@@ -4,10 +4,10 @@ import pathlib
 class PathHelper:
     @staticmethod
     def to_absolute(relative: str) -> str:
-        return str(pathlib.PurePath(relative).resolve())
+        return str(pathlib.Path(relative).resolve())
 
     @staticmethod
     def replace_suffix(input_path: str, extension: str) -> str:
         if not input_path:
             return ''
-        return str(pathlib.PurePath(input_path).with_suffix(extension))
+        return str(pathlib.Path(input_path).with_suffix(extension))
