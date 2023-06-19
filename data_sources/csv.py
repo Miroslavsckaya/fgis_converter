5243d594f1b4c2ffd2b0a9a45a0f773ab79a9498
mitsuha_s <stmiroslavsckaya@ya.ru>
2023-06-19 11:15:26 +0300

    bug fix

--- data_sources/csv.py
+++ data_sources/csv.py
@@ -27,7 +27,9 @@ class CsvDataSource(BaseFileDataSource, DataSourceInterface):
         """Expected order:
             1) registration number, 2) factory number, 3) modification, 4) verification date, 5) valid date,
             6) metrologist, 7) test device number, 8) temperature, 9) pressure, 10) humidity"""
-        if not all(row[:4] + row[5:]):
+        if not all(row[:4] + row[5:10]):
             logging.warning(f'Строка №{line_num} не может быть cконвертирована. Все поля должны быть заполнены')
             return None
-        return VerificationData(*row)
+        return VerificationData(reg_num=row[0], factory_num=row[1], modification=row[2], ver_date=row[3], 
+                                valid_date=row[4], metrologist=row[5], test_dev_num=row[6], temperature=row[7], 
+                                pressure=row[8], humidity=row[9])
