da4d35f9646614326ae286d14c6c5698f1e3bae5
mitsuha_s <stmiroslavsckaya@ya.ru>
2025-01-12 20:04:00 +0300

    change yml configuration examples for better ux

--- config.py
+++ config.py
@@ -1,18 +1,11 @@
 import yaml
 
-try:
-    config_file = open('./config/company_config.local.yml')
-except:
-    config_file = open('./config/company_config.yml')
-
+config_file = open('config/company_config.yml')
 company_config = yaml.safe_load(config_file)
 config_file.close()
 
 valid_regnums = {}
-try:
-    regnums_file = open('./config/registration_numbers.local.yml')
-except:
-    regnums_file = open('./config/registration_numbers.yml')
+regnums_file = open('config/registration_numbers.yml')
 
 for methodic, regnums in yaml.safe_load(regnums_file).items():
     for regnum in regnums:
